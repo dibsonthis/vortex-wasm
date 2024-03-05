bd2dce7a3570b330b5b90acb0d689c9b1d55a7d1
Adib Attie <adib@192-168-1-106.tpgi.com.au>
2024-03-05 22:31:07 +1100

    optimised size

--- wasm/compile.sh
+++ wasm/compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-.wasm/emscripten/tools/file_packager.py .wasm/vortex.data --preload src --js-output=.wasm/loader.js --from-emcc
+wasm/emscripten/tools/file_packager.py wasm/vortex.data --preload src --js-output=wasm/loader.js --from-emcc
